b22a8e462e5cc9a29820ca1397139990c3f3adca
Stephen Valdinger <stephen@chocolatey.io>
2021-05-26 12:02:16 -0400

    Add Remove-NexusRepository cmdlet Adds the ability to remove a given Nexus repository and associated documentation

---
+++ src/public/Remove-NexusRepository.ps1
@@ -0,0 +1,80 @@
+function Remove-NexusRepository {
+    <#
+    .SYNOPSIS
+    Removes a given repository from the Nexus instance
+    
+    .DESCRIPTION
+    Removes a given repository from the Nexus instance
+    
+    .PARAMETER Repository
+    The repository to remove
+    
+    .PARAMETER Force
+    Disable prompt for confirmation before removal
+    
+    .EXAMPLE
+    Remove-NexusRepository -Repository ProdNuGet
+
+    .EXAMPLE
+    Remove-NexusRepository -Repository MavenReleases -Force
+    #>
+    [CmdletBinding(HelpUri = 'https://github.com/steviecoaster/TreasureChest/blob/develop/docs/Remove-NexusRepository.md', SupportsShouldProcess, ConfirmImpact = 'High')]
+    Param(
+        [Parameter(Mandatory)]
+        [ArgumentCompleter({
+            param($command,$WordToComplete,$CommandAst,$FakeBoundParams)
+            $repositories = (Get-NexusRepository).Name
+
+            if($WordToComplete){
+                $repositories.Where{$_ -match "^$WordToComplete"}
+            } else {
+                $repositories
+            }
+        })]
+        [String[]]
+        $Repository,
+
+        [Parameter()]
+        [Switch]
+        $Force
+    )
+    begin {
+
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+
+        $urislug = "/service/rest/v1/repositories"
+    }
+    process {
+        $Uri = $urislug + "/$Name"
+
+        try {
+           
+            if ($Force -and -not $Confirm) {
+                $ConfirmPreference = 'None'
+                if ($PSCmdlet.ShouldProcess("$name", "Remove Repository")) {
+                    $result = Invoke-Nexus -UriSlug $Uri -Method 'DELETE' -ErrorAction Stop
+                    [pscustomobject]@{
+                        Status     = 'Success'
+                        Repository = $Name        
+                    }
+                }
+            }
+            else {
+                if ($PSCmdlet.ShouldProcess("$name", "Remove Repository")) {
+                    $result = Invoke-Nexus -UriSlug $Uri -Method 'DELETE' -ErrorAction Stop
+                    [pscustomobject]@{
+                        Status     = 'Success'
+                        Repository = $Name
+                        Timestamp  = $result.date
+                    }
+                }
+            }
+        }
+
+        catch {
+            $_.exception.message
+        }
+    }
+}

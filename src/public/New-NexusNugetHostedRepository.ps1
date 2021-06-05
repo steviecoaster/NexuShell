266df4c415b016898253db1fca25e6f81f4f88c6
Stephen Valdinger <stephen@chocolatey.io>
2021-06-04 21:57:13 -0400

    Split creating nuget hosted repos to their own function and doc

---
+++ src/public/New-NexusNugetHostedRepository.ps1
@@ -0,0 +1,124 @@
+function New-NexusNugetHostedRepository {
+    <#
+    .SYNOPSIS
+    Creates a new NuGet Hosted repository
+    
+    .DESCRIPTION
+    Creates a new NuGet Hosted repository
+    
+    .PARAMETER Name
+    The name of the repository
+    
+    .PARAMETER CleanupPolicy
+    The Cleanup Policies to apply to the repository
+    
+
+    
+    .PARAMETER Online
+    Marks the repository to accept incoming requests
+    
+    .PARAMETER BlobStoreName
+    Blob store to use to store NuGet packages
+    
+    .PARAMETER StrictContentValidation
+    Validate that all content uploaded to this repository is of a MIME type appropriate for the repository format
+    
+    .PARAMETER DeploymentPolicy
+    Controls if deployments of and updates to artifacts are allowed
+    
+    .PARAMETER HasProprietaryComponents
+    Components in this repository count as proprietary for namespace conflict attacks (requires Sonatype Nexus Firewall)
+    
+    .EXAMPLE
+    New-NexusNugetHostedRepository -Name NugetHostedTest -DeploymentPolicy Allow
+
+    .EXAMPLE
+
+    $RepoParams = @{
+        Name = MyNuGetRepo
+        CleanupPolicy = '90 Days'
+        DeploymentPolicy = 'Allow'
+        UseStrictContentValidation = $true
+    }
+    
+    New-NexusNugetHostedRepository @RepoParams
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding(HelpUri = 'https://github.com/steviecoaster/TreasureChest/blob/develop/docs/New-NexusNugetHostedRepository.md')]
+    Param(
+        [Parameter(Mandatory)]
+        [String]
+        $Name,
+
+        [Parameter()]
+        [String]
+        $CleanupPolicy,
+
+        [Parameter()]
+        [ValidateSet('True', 'False')]
+        [String]
+        $Online = 'True',
+
+        [Parameter()]
+        [String]
+        $BlobStoreName = 'default',
+
+        [Parameter()]
+        [ValidateSet('True', 'False')]
+        [String]
+        $UseStrictContentValidation = 'True',
+
+        [Parameter()]
+        [ValidateSet('Allow', 'Deny', 'Allow_Once')]
+        [String]
+        $DeploymentPolicy,
+
+        [Parameter()]
+        [Switch]
+        $HasProprietaryComponents
+    )
+
+    begin {
+
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+
+        $urislug = "/service/rest/v1/repositories"
+
+    }
+
+    process {
+        $formatUrl = $urislug + '/nuget'
+
+        $FullUrlSlug = $formatUrl + '/hosted'
+
+
+        $body = @{
+            name    = $Name
+            online  = $Online
+            storage = @{
+                blobStoreName               = $BlobStoreName
+                strictContentTypeValidation = $UseStrictContentValidation
+                writePolicy                 = $DeploymentPolicy
+            }
+            cleanup = @{
+                policyNames = @($CleanupPolicy)
+            }
+        }
+
+        if ($HasProprietaryComponents) {
+            $Prop = @{
+                proprietaryComponents = 'True'
+            }
+    
+            $Body.Add('component', $Prop)
+        }
+
+        Write-Verbose $($Body | ConvertTo-Json)
+        Invoke-Nexus -UriSlug $FullUrlSlug -Body $Body -Method POST
+
+    }
+}

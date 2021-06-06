339ae956b24d1010565c2c6f9bab144354d9fd08
Stephen Valdinger <stephen@chocolatey.io>
2021-06-05 20:52:02 -0400

    Added Cocoa,Conan,Conda,Docker support and docs

--- src/public/New-NexusDockerHostedRepository.ps1
+++ src/public/New-NexusDockerHostedRepository.ps1
@@ -1,10 +1,10 @@
-function New-NexusNugetHostedRepository {
+function New-NexusDockerHostedRepository {
     <#
     .SYNOPSIS
-    Creates a new NuGet Hosted repository
+    Creates a new Docker Hosted repository
     
     .DESCRIPTION
-    Creates a new NuGet Hosted repository
+    Creates a new Docker Hosted repository
     
     .PARAMETER Name
     The name of the repository
@@ -12,13 +12,11 @@ function New-NexusNugetHostedRepository {
     .PARAMETER CleanupPolicy
     The Cleanup Policies to apply to the repository
     
-
-    
     .PARAMETER Online
     Marks the repository to accept incoming requests
     
     .PARAMETER BlobStoreName
-    Blob store to use to store NuGet packages
+    Blob store to use to store Docker packages
     
     .PARAMETER StrictContentValidation
     Validate that all content uploaded to this repository is of a MIME type appropriate for the repository format
@@ -29,24 +27,39 @@ function New-NexusNugetHostedRepository {
     .PARAMETER HasProprietaryComponents
     Components in this repository count as proprietary for namespace conflict attacks (requires Sonatype Nexus Firewall)
     
+    .PARAMETER EnableV1
+    Whether to allow clients to use the V1 API to interact with this repository
+    
+    .PARAMETER ForceBasicAuth
+    Whether to force authentication (Docker Bearer Token Realm required if false)
+    
+    .PARAMETER HttpPort
+    Create an HTTP connector at specified port
+    
+    .PARAMETER HttpsPort
+    Create an HTTPS connector at specified port
+    
     .EXAMPLE
-    New-NexusNugetHostedRepository -Name NugetHostedTest -DeploymentPolicy Allow
+    New-NexusDockerHostedRepository -Name DockerHostedTest -DeploymentPolicy Allow -EnableV1 -ForceBasicAuth
 
     .EXAMPLE
 
     $RepoParams = @{
-        Name = MyNuGetRepo
+        Name = MyDockerRepo
         CleanupPolicy = '90 Days'
         DeploymentPolicy = 'Allow'
         UseStrictContentValidation = $true
+        ForceBasicAuth = $true
+        HttpPort = '8082'
+        EnableV1 = $true
     }
     
-    New-NexusNugetHostedRepository @RepoParams
+    New-NexusDockerHostedRepository @RepoParams
 
+    
     .NOTES
-    General notes
     #>
-    [CmdletBinding(HelpUri = 'https://github.com/steviecoaster/TreasureChest/blob/develop/docs/New-NexusNugetHostedRepository.md')]
+    [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
         [String]
@@ -76,7 +89,23 @@ function New-NexusNugetHostedRepository {
 
         [Parameter()]
         [Switch]
-        $HasProprietaryComponents
+        $HasProprietaryComponents,
+
+        [Parameter()]
+        [Switch]
+        $EnableV1,
+
+        [Parameter()]
+        [Switch]
+        $ForceBasicAuth,
+
+        [Parameter()]
+        [String]
+        $HttpPort,
+        
+        [Parameter()]
+        [String]
+        $HttpsPort
     )
 
     begin {
@@ -84,16 +113,13 @@ function New-NexusNugetHostedRepository {
         if (-not $header) {
             throw "Not connected to Nexus server! Run Connect-NexusServer first."
         }
-
-        $urislug = "/service/rest/v1/repositories"
-
+    
+        $urislug = "/service/rest/v1/repositories/docker/hosted"
+    
     }
 
+    
     process {
-        $formatUrl = $urislug + '/nuget'
-
-        $FullUrlSlug = $formatUrl + '/hosted'
-
 
         $body = @{
             name    = $Name
@@ -106,6 +132,13 @@ function New-NexusNugetHostedRepository {
             cleanup = @{
                 policyNames = @($CleanupPolicy)
             }
+            docker = @{
+                v1Enabled = [bool]$EnableV1
+                forceBasicAuth = [bool]$ForceBasicAuth
+                httpPort = $HttpPort
+                httpsPort = $HttpsPort
+            }
+
         }
 
         if ($HasProprietaryComponents) {
@@ -116,8 +149,14 @@ function New-NexusNugetHostedRepository {
             $Body.Add('component', $Prop)
         }
 
-        Write-Verbose $($Body | ConvertTo-Json)
-        Invoke-Nexus -UriSlug $FullUrlSlug -Body $Body -Method POST
 
+        Write-Verbose $($Body | ConvertTo-Json)
+        Invoke-Nexus -UriSlug $urislug -Body $Body -Method POST
+
+        if($ForceBasicAuth -eq $false){
+            Write-Warning "Docker Bearer Token Realm required since -ForceBasicAuth was not passed."
+            Write-Warning "Use Add-NexusRealm to enable if desired."
+        }
+    
     }
 }

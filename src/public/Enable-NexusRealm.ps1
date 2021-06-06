984d03378b6552fd4e17f669272445bc15ae7aac
Stephen Valdinger <stephen@chocolatey.io>
2021-06-06 11:06:02 -0400

    Add Enable-NexusRealm and docs

---
+++ src/public/Enable-NexusRealm.ps1
@@ -0,0 +1,79 @@
+function Enable-NexusRealm {
+    <#
+    .SYNOPSIS
+    Enable realms in Nexus
+    
+    .DESCRIPTION
+    Enable realms in Nexus
+    
+    .PARAMETER Realm
+    The realms you wish to activate
+    
+    .EXAMPLE
+    Enable-NexusRealm -Realm 'NuGet Api-Key Realm', 'Rut Auth Realm'
+
+    .EXAMPLE
+    Enable-NexusRealm -Realm 'LDAP Realm'
+    
+    .NOTES
+    #>
+    [CmdletBinding(elpUri = 'https://github.com/steviecoaster/TreasureChest/blob/develop/docs/Enable-NexusRealm.md')]
+    Param(
+        [Parameter(Mandatory)]
+        [ArgumentCompleter({
+            param($Command,$Parameter,$WordToComplete,$CommandAst,$FakeBoundParams)
+
+            $r = (Get-NexusRealm).name
+
+            if($WordToComplete){
+                $r.Where($_ -match "^$WordToComplete")
+            } else {
+                $r
+            }
+        }
+        )]
+        [String[]]
+        $Realm
+    )
+
+    begin {
+
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+
+        $urislug = "/service/rest/v1/security/realms/active"
+
+    }
+
+    process {
+
+        $collection = @()
+
+        Get-NexusRealm -Active | ForEach-Object { $collection += $_.id }
+
+        $Realm | Foreach-Object {
+
+            switch($_){
+                'Conan Bearer Token Realm' { $id = 'org.sonatype.repository.conan.internal.security.token.ConanTokenRealm' }
+                'Default Role Realm' { $id = 'DefaultRole' }
+                'Docker Bearer Token Realm' { $id = 'DockerToken' }
+                'LDAP Realm' { $id = 'LdapRealm' }
+                'Local Authentication Realm' { $id = 'NexusAuthenticatingRealm'}
+                'Local Authorizing Realm' {$id = 'NexusAuthorizingRealm'}
+                'npm Bearer Token Realm' {$id = 'NpmToken'}
+                'NuGet API-Key Realm' { $id = 'NuGetApiKey'}
+                'Rut Auth Realm' { $id = 'rutauth-realm'}
+            }
+
+            $collection += $id
+    
+        }
+
+        $body = $collection
+
+        Write-Verbose $($Body | ConvertTo-Json)
+        Invoke-Nexus -UriSlug $urislug -BodyAsArray $Body -Method PUT
+
+    }
+}

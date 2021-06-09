5878e4c53b2bbfd064f05578b649fe6f0cafeb65
Stephen Valdinger <stephen@chocolatey.io>
2021-06-09 08:58:27 -0400

    Publish Privileges api commands

---
+++ src/public/Security/Privileges/Get-NexusPrivilege.ps1
@@ -0,0 +1,74 @@
+function Get-NexusPrivilege {
+    <#
+    .SYNOPSIS
+    Retrieve Privilege information from Nexus
+    
+    .DESCRIPTION
+    Retrieve Privilege information from Nexus
+    
+    .PARAMETER PrivilegeId
+    The id of the privilege to retrieve.
+    
+    .EXAMPLE
+    Get-NexusPrivilege
+
+    .EXAMPLE
+    Get-NexusPrivilege -PrivilegeId NuGetProdAdmin
+    
+    .NOTES
+    
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [String]
+        $PrivilegeId
+    )
+
+    begin {
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+    }
+
+    process {
+
+
+        if ($PrivilegeId) {
+            $urislug = "/service/rest/v1/security/privileges/$PrivilegeId"
+
+            try {
+                $result = Invoke-Nexus -Urislug $urislug -Method GET
+
+                [pscustomobject]@{
+                    Name        = $result.name
+                    Type        = $result.type
+                    Description = $result.description
+                    ReadOnly    = $result.readonly
+                    Actions     = $result.actions
+                    Domain      = $result.domain
+                }
+            }
+            catch {
+                $_.Exception.Message
+            }
+        }
+        else {
+            $urislug = '/service/rest/v1/security/privileges'
+
+            $result = Invoke-Nexus -Urislug $urislug -Method GET
+
+            $result | Foreach-Object {
+                [pscustomobject]@{
+                    Name        = $_.name
+                    Type        = $_.type
+                    Description = $_.description
+                    ReadOnly    = $_.readOnly
+                    Pattern     = $_.pattern
+                }
+            }
+    
+        }
+
+    }
+}
---
+++ src/public/Security/Privileges/Remove-NexusPrivilege.ps1
@@ -0,0 +1,68 @@
+function Remove-NexusPrivilege {
+    <#
+    .SYNOPSIS
+    Remove a Nexus Privilege
+    
+    .DESCRIPTION
+    Remove a Nexus Privilege
+    
+    .PARAMETER PrivilegeId
+    The id of the privilege to delete.
+
+    .PARAMETER Force
+    Don't prompt for confirmation before removal
+    
+    .EXAMPLE
+    Remove-NexusPrivilege -PrivilegeId NuGetSuperAdmin
+
+    .EXAMPLE
+    Get-NexusPrivilege -PrivilegeId NuGetSuperAdmin | Remove-NexusPrivilege
+
+    .EXAMPLE
+    Remove-NexusPrivilege -PrivilegeId NuGetSuperAdmin -Force
+    
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+    Param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [ArgumentCompleter({
+            param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
+
+            $r = (Get-NexusPrivilege).Name
+
+            if($WordToComplete){
+                $r.Where($_ -match "^$WordToComplete")
+            }
+            else {
+                $r
+            }
+        })]
+        [String[]]
+        [Alias('Name')]
+        $PrivilegeId,
+
+        [Parameter()]
+        [Switch]
+        $Force
+    )
+
+    process {
+        $PrivilegeId | Foreach-Object {
+            $urislug = "/service/rest/v1/security/privileges/$($_)"
+
+            if ($Force -and -not $Confirm) {
+                $ConfirmPreference = 'None'
+                if ($PSCmdlet.ShouldProcess("$($_)", "Remove Privilege")) {
+                    Invoke-Nexus -UriSlug $urislug -Method DELETE -ErrorAction Stop
+                }
+            }
+            else {
+                if ($PSCmdlet.ShouldProcess("$($_)", "Remove Privilege")) {
+                    Invoke-Nexus -UriSlug $urislug -Method DELETE -ErrorAction Stop
+                }
+            }
+        }
+    }
+}

c55ba35606423bd771cb3f439745ef980532c8fa
James Ruskin <james@chocolatey.io>
2021-07-04 13:39:47 +0100

    Sketches TestPrePublish Step

--- build.ps1
+++ build.ps1
@@ -39,20 +39,29 @@ process {
         }
 
         $TestPrePublish {
-            
-            Get-ChildItem $root\src\public\*.ps1, $root\Poshbot\*.ps1 | 
-            Foreach-Object { 
-                . $_.FullName
+            if (Test-Path $root\Output\TreasureChest) {
+                if ($env:PSModulePath.Split(';') -notcontains "$root\Output") {
+                    $env:PSModulePath = "$root\Output;$env:PSModulePath"
+                }
+                Import-Module TreasureChest
             }
-            #Import-Module "$root\Output\TreasureChest\TreasureChest.psd1" -Force
-            Import-Module PoshBot -Force
 
-            Invoke-Pester "$root\tests\pre"
-            
+            $TestArguments = @{
+                Path                   = "$root\tests"
+
+                OutputFile             = "$root\TestResults.xml"
+                OutputFormat           = "JUnitXml"
+                
+                CodeCoverage           = (Get-ChildItem $root\Output\TreasureChest -Recurse -Filter '*.ps*1').FullName
+                CodeCoverageOutputFile = "$root\Coverage.xml"
+            }
+
+            if (Test-Path $TestArguments.Path) {
+                Invoke-Pester @TestArguments
+            }
         }
 
         $TestPostPublish {
-
             Install-Module TreasureChest -Force
             Import-Module PoshBot -Force
 
@@ -60,22 +69,18 @@ process {
         }
 
         $DeployToGallery {
-
             Publish-Module -Path "$root\Output\TreasureChest" -NuGetApiKey $env:NugetApiKey
-
         }
 
         $Choco {
-            
             $Nuspec = Get-ChildItem "$root\src\nuget" -recurse -filter *.nuspec
 
             if (Test-Path "$root\src\nuget\tools\TreasureChest.zip") {
                 choco pack $Nuspec.Fullname --output-directory $Nuspec.directory
-            }
-
-            else {
+            } else {
                 throw "Welp, ya need the zip in the tools folder, dumby"
             }
+            
             Get-ChildItem "$root\src\nuget" -recurse -filter *.nupkg | 
             Foreach-Object { 
                 choco push $_.FullName -s https://push.chocolatey.org --api-key="'$($env:ChocoApiKey)'"

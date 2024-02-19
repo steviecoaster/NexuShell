215193d9b907b3fd83f30fb9400ed18786c8db0a
Stephen Valdinger <stephen@chocolatey.io>
2024-02-19 15:00:46 -0500

    Feature: Tag support
    This commit adds the ability to manage tags with
    your Sonatype Nexus repository installation

---
+++ src/public/Tags/Get-NexusTag.ps1
@@ -0,0 +1,33 @@
+function Get-NexusTag {
+    [Cmdletbinding(HelpUri = 'https://steviecoaster.github.io/NexuShell/Tags/Get-NexusTag/')]
+    Param(
+        [Parameter()]
+        [String]
+        $Name
+    )
+
+    begin {
+
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+
+        $urislug = if (-not $Name) {
+            '/service/rest/v1/tags'
+        }
+        else {
+            "/service/rest/v1/tags/$Name"
+        }
+
+    }
+
+    process {
+        if(-not $Name){
+            $result = Invoke-Nexus -Urislug $urislug -Method GET
+            $result.items
+        } else {
+            Invoke-Nexus -UriSlug $urislug -Method GET
+        }
+    }
+ 
+}
---
+++ src/public/Tags/New-NexusTag.ps1
@@ -0,0 +1,70 @@
+function New-NexusTag {
+<#
+.SYNOPSIS
+Create a tag
+
+.DESCRIPTION
+Create a tag in Nexus Repository. Useful with CI/CD platforms, similar to git tags.
+
+.PARAMETER Tag
+The friendly name of the tag
+
+.PARAMETER Attributes
+Any additional metadata you wish to tag a component with
+
+.EXAMPLE
+ $tag = @{
+    name = 'SampleTag'
+    attributes = @{
+        jvm = '9'
+        builtby = 'Jenkins'
+    }
+ }
+
+ New-NexusTag @tag
+
+.LINK
+https://help.sonatype.com/en/tagging.html
+#>
+    [CmdletBinding(HelpUri='https://steviecoaster.github.io/NexuShell/Tags/New-NexusTag/')]
+    Param(
+        [Parameter(Mandatory)]
+        [ValidateLength(1,256)]
+        [String]
+        $Tag,
+
+        [Parameter()]
+        [hashtable]
+        $Attributes
+    )
+    begin{
+        if (-not $header) {
+            throw "Not connected to Nexus server! Run Connect-NexusServer first."
+        }
+        
+        $urislug = '/service/rest/v1/tags'
+
+        if ($Tag.Length -gt 256) {
+            throw "Name cannot exceed 256 characters."
+        }
+        
+        if ($Tag -notmatch '^[a-zA-Z0-9_.-]+$') {
+            throw "Name can only contain letters, numbers, underscores, hyphens, and dots."
+        }
+        
+        if ($Tag -match '^[_\.]') {
+            throw "Name cannot start with an underscore or dot."
+        }
+    }
+
+    process {
+
+        $body = @{
+            name = $Tag
+            attributes = $Attributes
+        }
+
+        Invoke-Nexus -Urislug $urislug -Body $body -Method POST
+    }
+
+}
